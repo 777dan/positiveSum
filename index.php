5a9748f2e12314bd3d817632dc0b26434638488a
777dan <danya.nikitin7@gmail.com>
2022-09-28 08:52:06 +0300

    second commit

--- index.php
+++ index.php
@@ -8,18 +8,10 @@ function test_positive($var)
 {
     return $var > 0;
 }
-// for ($i = 0; $i < count($arrNum); $i++) {
-//     $filter = array_filter($arrNum[$i], "test_positive");
-//     for ($j = 0; $j < count($arrNum[$i]); $j++) {
-//         $sum = array_sum($filter);
-//     }
-// }
-// echo $sum;
-// // print_r(array_sum($filter));
-// echo(array_sum(array_filter($$arrNum[$i],"test_odd")));
 for ($i = 0; $i < count($arrNum); $i++) {
-    // for ($j = 0; $j < count($arrNum[$i]); $j++) {
-    // // echo(array_sum(array_filter($arrNum[$i],"test_positive")));
-    // }
-    echo(array_sum(array_filter($arrNum[$i],"test_positive")));
+    $filt = array_filter($arrNum[$i],"test_positive");
+    $sum1 = array_sum($filt);
+    $positive[$i] = $sum1;
     }
+    $sum2 = array_sum($positive);
+    echo $sum2;
